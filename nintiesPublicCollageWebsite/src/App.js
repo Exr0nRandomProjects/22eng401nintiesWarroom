fc1bbb374507c773c233bec79e22580916acbefa
Exr0n <mail@exr0n.com>
2022-09-29 13:25:01 -0700

    images from day 1

--- nintiesPublicCollageWebsite/src/App.js
+++ nintiesPublicCollageWebsite/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import './tailwind.css';
 import TimelapseSlider from './TimelapseSlider.jsx';
 
-const IMAGE_URLS = [ "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=640:*", "https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80"];
+const IMAGE_URLS = [ "/timelapse_images/day1-working.jpeg", "/timelapse_images/day1-complete.jpeg"];
 
 function App() {
     return (
         <div className="w-screen min-h-screen overflow-x-hidden bg-gray-900 scroll-x-none">
             <div className="flex flex-col items-center w-full p-6">
-                <div className="flex">
-                    <span className="justify-center text-5xl font-semibold text-pink-500">The Ninties</span>
+                <div className="flex justify-center text-pink-400 align-text-bottom" style={{ fontFamily: "'Lobster', cursive" }}>
+                    <div style={{ fontSize: '80px' }} className="font-semibold">The Ninties</div>
+        {/*<div style={{ fontSize: '30px' }}>Allen&apos;s English Class</div>*/}
                 </div>
-                <div className="border border-2 border-red-400">
+                <div>
                     <TimelapseSlider image_urls={IMAGE_URLS} />
                 </div>
             </div>
--- nintiesPublicCollageWebsite/src/TimelapseSlider.jsx
+++ nintiesPublicCollageWebsite/src/TimelapseSlider.jsx
@@ -16,11 +16,11 @@ export default function TimelapseSlider({ image_urls }) {
         setPhotoId(image_urls.length - value -1)
     }
 
-    return <div className="flex items-center border border-2 border-red">
+    return <div className="flex items-center">
         <div style={{ width: `${win_width}px`, height: `${win_width/2}px` }} className="flex items-center overflow-hidden align-center">
             <img className="w-full overflow-hidden" src={image_urls[photo_id]} />
         </div>
-        <div className="border-2 border-red-300" style={{ height: `${win_width/2}px` }}>
+        <div style={{ height: `${win_width/2}px` }}>
             <Slider
                 orientation="vertical"
                 defaultValue={image_urls.length - default_index}
